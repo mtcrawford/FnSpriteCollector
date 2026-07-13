935c1d39da3d44a82a93e0d50029302dce057e0f
Matthew Crawford <mtcrawford@putnamco.com>
2026-07-13 13:37:26 -0500

    Added IsReleased property

--- src/FNSpriteCollector.Shared/Models/FnSprite.cs
+++ src/FNSpriteCollector.Shared/Models/FnSprite.cs
@@ -3,10 +3,12 @@
 public class FnSprite
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public SpriteRarity Rarity { get; set; }
     public SpriteVariant Variant { get; set; }
     public SpriteFamily Family { get; set; }
     public string ImageBase64 { get; set; } = string.Empty;
+
+    public bool IsReleased { get; set; } = true;
 }
 
